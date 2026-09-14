32eec41fa38f11955a7773aa10c6435c011b135d
Ugie01 <dlauddnr1@gmail.com>
2026-09-14 19:15:59 +0900

    feat: integrate ugie backend and robot modules with sein-ui frontend

---
+++ robots/src/zenoh_pkg/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = 'zenoh_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml']
+        ),
+        (
+            os.path.join('share', package_name, 'launch'),
+            glob('launch/*.launch.py')
+        ),
+        (
+            os.path.join('share', package_name, 'config'),
+            glob('config/*.json5')
+        ),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='turtlebot',
+    maintainer_email='turtlebot@example.com',
+    description='Zenoh communication package',
+    license='Apache-2.0',
+    entry_points={
+        'console_scripts': [
+            'robot_agent_test = zenoh_pkg.robot_agent_test:main',
+        ],
+    },
+)
